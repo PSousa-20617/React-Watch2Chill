f2d022c4b7d8ec3ca1d545d900532ecb902163c2
Pedrocos17 <Pedrocos17@users.noreply.github.com>
2021-07-12 20:12:39 +0100

    update de trabalho

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import NavRouter from './components/NavRouter'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+//npx json-server --watch ./src/assets/videosBD.json --port 8000
 function App() {
   return (
     <NavRouter></NavRouter>
---
+++ src/pages/Filmes.js
@@ -0,0 +1,32 @@
+import React, { useEffect,useState } from 'react'
+import moviesDB from '../assets/videosBD.json'
+import logo from '../assets/logos/logo.png'
+import background from '../assets/Background/background.png'
+import Filme from '../components/Filme.js'
+import "./Filmes.css"
+
+
+const Filmes = () => {
+
+const [filmes,setFilmes] = useState()
+
+
+useEffect(() => {
+    fetch("http://localhost:8000/movies")
+    .then(res =>{
+      return res.json();
+    })
+    .then(data => {
+      setFilmes(data);
+      console.log(data);
+    })
+  }, [])
+
+    return (
+        <body>
+        {filmes && filmes.map((filme)=><Filme key={filme.id} data = {filme}></Filme> )}
+        </body>
+    )
+}
+
+export default Filmes
